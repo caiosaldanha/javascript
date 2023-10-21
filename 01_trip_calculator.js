e85c8b3a490d8aeebaf869999eb79aee7bf4f8f4
Caio Saldanha <caiowrite@gmail.com>
2023-10-20 22:11:01 -0300

    simpler version of trip calculator

--- 01_trip_calculator.js
+++ 01_trip_calculator.js
@@ -1,17 +1,21 @@
-//This is a simple program to calculate the planned expense of a trip
-//Written by Caio Saldanha
+/*
+===================================================================
+This is a simple program to calculate the planned expense of a trip
+Written by Caio Saldanha
+===================================================================
+*/
 
-//start the program
-console.log('3 questions to calculate the trip:');
+//What is the fuel price per liter?
+let fuel_price = 5;
 
-//variables collection
-var fuel_price = prompt("1/3 - What is the fuel price per liter?");
-var car_consumption = prompt("2/3 - What's KM per liter car consumption?");
-var distance_km = prompt("3/3 - What is the distance in KMs?");
+//What's KM per liter car consumption?
+let car_consumption = 10;
 
-//calculation
-var result = (distance_km / car_consumption) * fuel_price;
+//What is the distance in KMs?
+let distance_km = 100;
+
+//result calculation
+let result = (distance_km / car_consumption) * fuel_price;
 
 //print out the result
-console.log("You will need ${result} for the trip.");
- 
+console.log(`You will need ${result} bucks for the trip.`);
